5c2e5b54f89ac8de2115fa7af0f6aa58b5739abe
Simon Walker <s.r.walker101@googlemail.com>
2016-06-28 22:29:34 +0100

    Add header file

---
+++ c_src/batman.h
@@ -0,0 +1,17 @@
+#ifndef BATMAN_H_
+#define BATMAN_H_
+
+// _rsky.c
+double getE(double M, double e);
+void rsky(double *ts, double *ds, int len, double tc, double per, double a, double inc, double ecc, double omega, int transittype);
+void getf(double *ts, double *fs, int len, double tc, double per, double a, double inc, double ecc, double omega, int transittype);
+
+// _eclipse.c
+void eclipse(double *ds, double *fs, int len, double p, double fp, int nthreads);
+
+// _nonlinear.c
+double intensity(double x, double c1, double c2, double c3, double c4, double norm);
+double area(double d, double x, double R);
+void nonlinear_ld(double *ds, double *fs, int len, double rprs, double c1, double c2, double c3, double c4, double fac, int nthreads);
+
+#endif //  BATMAN_H_
