75889682d7c0688aa021ff6783d035bc06758d9d
Simon Walker <s.r.walker101@googlemail.com>
2016-07-06 16:56:58 +0100

    Add logarithmic ld support

--- c_src/_logarithmic_ld.c
+++ c_src/_logarithmic_ld.c
@@ -1,23 +1,26 @@
 /* The batman package: fast computation of exoplanet transit light curves
- * Copyright (C) 2015 Laura Kreidberg	 
- * 
+ * Copyright (C) 2015 Laura Kreidberg
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef BATMAN_PYTHON
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <Python.h>
 #include "numpy/arrayobject.h"
+#endif
+
 #include <math.h>
 
 #if defined (_OPENMP)
@@ -31,22 +34,24 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
+#ifdef BATMAN_PYTHON
 static PyObject *_logarithmic_ld(PyObject *self, PyObject *args);
+#endif
 
-double intensity(double x, double c1, double c2, double norm)
+static double intensity(double x, double c1, double c2, double norm)
 {
 	if(x > 0.99995) x = 0.99995;
 	double mu = sqrt(1. - x*x);
-	return (1. - c1*(1.-mu) - c2*mu*log(mu))/norm; 
+	return (1. - c1*(1.-mu) - c2*mu*log(mu))/norm;
 }
 
-double area(double d, double x, double R)
+static double area(double d, double x, double R)
 {
 	/*
 	Returns area of overlapping circles with radii r and R; separated by a distance d
 	*/
-	double arg1 = (d*d + x*x - R*R)/(2.*d*x); 	
-	double arg2 = (d*d + R*R - x*x)/(2.*d*R); 
+	double arg1 = (d*d + x*x - R*R)/(2.*d*x);
+	double arg2 = (d*d + R*R - x*x)/(2.*d*R);
 	double arg3 = MAX((-d + x + R)*(d + x - R)*(d - x + R)*(d + x + R), 0.);
 
 	if(x <= R - d) return M_PI*x*x;						//planet completely overlaps stellar circle
@@ -54,17 +59,70 @@ double area(double d, double x, double R)
 	else return x*x*acos(arg1) + R*R*acos(arg2) - 0.5*sqrt(arg3);		//partial overlap
 }
 
+void logarithmic_ld(double *ds, double *fs, int len, double rprs, double c1, double c2, double fac, int nthreads) {
+
+  double d, A_i, x, I;
+  int i;
+  double dx, A_f, x_in, x_out, delta;
+
+#if defined (_OPENMP)
+  omp_set_num_threads(nthreads);	//specifies number of threads (if OpenMP is supported)
+#endif
+
+  double norm = (-3.*c1 + 2.*c2 + 9.)*M_PI/9.0; 	//normalization for intensity profile (faster to calculate it once, rather than every time intensity is called)
+
+#if defined (_OPENMP)
+#pragma omp parallel for private(d, x_in, x_out, delta, x, dx, A_i, A_f, I)
+#endif
+  for(i = 0; i < len; i++)
+  {
+    d = ds[i];
+    x_in = MAX(d - rprs, 0.);					//lower bound for integration
+    x_out = MIN(d + rprs, 1.0);					//upper bound for integration
+
+    if(x_in >= 1.) fs[i] = 1.0;				//flux = 1. if the planet is not transiting
+    else
+    {
+      delta = 0.;						//variable to store the integrated intensity, \int I dA
+      x = x_in;						//starting radius for integration
+      dx = fac*acos(x); 					//initial step size
+
+      x += dx;						//first step
+
+      A_i = 0.;						//initial area
+
+      while(x < x_out)
+      {
+	A_f = area(d, x, rprs);				//calculates area of overlapping circles
+	I = intensity(x - dx/2., c1, c2, norm); 	//intensity at the midpoint
+	delta += (A_f - A_i)*I;				//increase in transit depth for this integration step
+	dx = fac*acos(x);  				//updating step size
+	x = x + dx;					//stepping to next element
+	A_i = A_f;					//storing area
+      }
+      dx = x_out - x + dx;  					//calculating change in radius for last step
+      x = x_out;						//final radius for integration
+      A_f = area(d, x, rprs);					//area for last integration step
+      I = intensity(x - dx/2., c1, c2, norm); 		//intensity at the midpoint
+      delta += (A_f - A_i)*I;					//increase in transit depth for this integration step
+
+      fs[i] = 1.0 - delta;	//flux equals 1 - \int I dA
+    }
+  }
+}
+
+#ifdef BATMAN_PYTHON
 static PyObject *_logarithmic_ld(PyObject *self, PyObject *args)
 {
-	double rprs, d, fac, A_i, x, I; 
+	double rprs, d, fac, A_i, x, I;
 	int nthreads;
 	npy_intp i, dims[1];
 	double dx, A_f, x_in, x_out, delta, c1, c2;
-	
+
 	PyArrayObject *ds, *flux;
   	if(!PyArg_ParseTuple(args,"Oddddi", &ds, &rprs, &c1, &c2, &fac, &nthreads)) return NULL;
 
-	dims[0] = PyArray_DIMS(ds)[0]; 
+	dims[0] = PyArray_DIMS(ds)[0];
 	flux = (PyArrayObject *) PyArray_SimpleNew(1, dims, PyArray_TYPE(ds));	//creates numpy array to store return flux values
 
 	double *f_array = PyArray_DATA(flux);
@@ -72,64 +130,21 @@ static PyObject *_logarithmic_ld(PyObject *self, PyObject *args)
 
 	/*
 		NOTE:  the safest way to access numpy arrays is to use the PyArray_GETITEM and PyArray_SETITEM functions.
-		Here we use a trick for faster access and more convenient access, where we set a pointer to the 
+		Here we use a trick for faster access and more convenient access, where we set a pointer to the
 		beginning of the array with the PyArray_DATA (e.g., f_array) and access elements with e.g., f_array[i].
 		Success of this operation depends on the numpy array storing data in blocks equal in size to a C double.
 		If you run into trouble along these lines, I recommend changing the array access to something like:
-			d = PyFloat_AsDouble(PyArray_GETITEM(ds, PyArray_GetPtr(ds, &i))); 
+			d = PyFloat_AsDouble(PyArray_GETITEM(ds, PyArray_GetPtr(ds, &i)));
 		where ds is a numpy array object.
 
 
 		Laura Kreidberg 07/2015
 	*/
-	
-	#if defined (_OPENMP)
-	omp_set_num_threads(nthreads);	//specifies number of threads (if OpenMP is supported)
-	#endif
+	logarithmic_ld(d_array, f_array, dims[0], rprs, c1, c2, fac, nthreads);
 
-	double norm = (-3.*c1 + 2.*c2 + 9.)*M_PI/9.0; 	//normalization for intensity profile (faster to calculate it once, rather than every time intensity is called)		
-
-	#if defined (_OPENMP)
-	#pragma omp parallel for private(d, x_in, x_out, delta, x, dx, A_i, A_f, I)
-	#endif
-	for(i = 0; i < dims[0]; i++)
-	{
-		d = d_array[i];
-		x_in = MAX(d - rprs, 0.);					//lower bound for integration
-		x_out = MIN(d + rprs, 1.0);					//upper bound for integration
-
-		if(x_in >= 1.) f_array[i] = 1.0;				//flux = 1. if the planet is not transiting
-		else
-		{
-			delta = 0.;						//variable to store the integrated intensity, \int I dA
-			x = x_in;						//starting radius for integration
-			dx = fac*acos(x); 					//initial step size 
-
-			x += dx;						//first step
-
-			A_i = 0.;						//initial area
-	
-			while(x < x_out)
-			{
-				A_f = area(d, x, rprs);				//calculates area of overlapping circles
-				I = intensity(x - dx/2., c1, c2, norm); 	//intensity at the midpoint
-				delta += (A_f - A_i)*I;				//increase in transit depth for this integration step
-				dx = fac*acos(x);  				//updating step size
-				x = x + dx;					//stepping to next element
-				A_i = A_f;					//storing area
-			}
-			dx = x_out - x + dx;  					//calculating change in radius for last step  
-			x = x_out;						//final radius for integration
-			A_f = area(d, x, rprs);					//area for last integration step
-			I = intensity(x - dx/2., c1, c2, norm); 		//intensity at the midpoint 
-			delta += (A_f - A_i)*I;					//increase in transit depth for this integration step
-
-			f_array[i] = 1.0 - delta;	//flux equals 1 - \int I dA 
-		}
-	}
 	return PyArray_Return((PyArrayObject *)flux);
 
-} 
+}
 
 static char _logarithmic_ld_doc[] = "This extension module returns a limb darkened light curve for a logarithmic stellar intensity profile.";
 
@@ -141,7 +156,7 @@ static PyMethodDef _logarithmic_ld_methods[] = {
 		PyModuleDef_HEAD_INIT,
 		"_logarithmic_ld",
 		_logarithmic_ld_doc,
-		-1, 
+		-1,
 		_logarithmic_ld_methods
 	};
 
@@ -153,7 +168,7 @@ static PyMethodDef _logarithmic_ld_methods[] = {
 		{
 			return NULL;
 		}
-		import_array(); 
+		import_array();
 		return module;
 	}
 #else
@@ -161,7 +176,8 @@ static PyMethodDef _logarithmic_ld_methods[] = {
 	void init_logarithmic_ld(void)
 	{
 	  	Py_InitModule("_logarithmic_ld", _logarithmic_ld_methods);
-		import_array(); 
+		import_array();
 	}
 #endif
 
+#endif // BATMAN_PYTHON
--- c_src/batman.h
+++ c_src/batman.h
@@ -16,6 +16,9 @@ void eclipse(double *ds, double *fs, int len, double p, double fp, int nthreads)
 // _exponential_ld.c
 void exponential_ld(double *ds, double *fs, int len, double rprs, double c1, double c2, double fac, int nthreads);
 
+// _logarithmic_ld.c
+void logarithmic_ld(double *ds, double *fs, int len, double rprs, double c1, double c2, double fac, int nthreads);
+
 // _nonlinear.c
 void nonlinear_ld(double *ds, double *fs, int len, double rprs, double c1, double c2, double c3, double c4, double fac, int nthreads);
 
